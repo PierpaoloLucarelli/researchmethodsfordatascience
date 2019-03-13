8ed5c71a5d850a2f34f27175fe134d02d17880bb
Pierpaolo <plucarelli.94@gmail.com>
2019-03-13 19:07:47 +0100

    remove cred

--- A/your_twitter.R
+++
@@ -1,5 +0,0 @@
-#adapt this file based on your twitter app
-consumer_key <-'TQ13YPovnX07US0QLPlH0aftY'
-consumer_scret <- 'AFB0cOq8Xn46cTeHHFq76ufoemnC69ZsgVc5MKt2D6bVk6pnd1'
-access_token <- '2294620860-iQCs3OjB968DOJgN9ye8F9IdH4yxwPI6TyN4dIl'
-access_scret <- 'EhYcW2Hi7p4p7ePaCcLeh1rSQLXtWiYKSJPXHoiGMH2mG'
