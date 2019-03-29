1c484f73256570b7197ca4343ece8ea08493b9af
NavinLR <lr.navin@yahoo.com>
2019-03-29 10:06:34 +0100

    graphs and comp time

--- C/tests.m
+++ C/tests.m
@@ -6,22 +6,38 @@ if data_set_choice == 0
     label = species;
 elseif data_set_choice == 1
     fashion_mnist = readtable('fashion_mnist_2.csv');
-    data = table2array(heart_data(:,2:end));
-    label = table2array(heart_data(:,1)); 
+    data = table2array(fashion_mnist(1:10000,2:end));
+    label = table2array(fashion_mnist(1:10000,1)); 
 else
     heart_data = readtable('heart.csv');
     data = table2array(heart_data(:,1:end-1));
     label = table2array(heart_data(:,end));
 end    
- 
-% remove the column names and non numeric columns
-%data = table2array(data(:,1:size(data, 2)-1));
 
+time1   = clock;
 [pca_data1, eigVecs] = mypca(data, 2);
+time2   = clock;
+fprintf('Covar-PCA:   %g\n', etime(time2, time1));
+
+time1   = clock;
 [pca_data2, eigVecs2] = gramPca(data, 2);
+time2   = clock;
+fprintf('Gram-PCA:   %g\n', etime(time2, time1));
+
+time1   = clock;
 [pca_data3, eigVecs3] = snapPca(data, 2, 1);
+time2   = clock;
+fprintf('Snap-PCA:   %g\n', etime(time2, time1));
+
+time1   = clock;
 [pca_data4, eigVecs4] = nystrom(data, 2);
+time2   = clock;
+fprintf('Nyst-PCA:   %g\n', etime(time2, time1));
+
+time1   = clock;
 sne_op = tsne(data);
+time2   = clock;
+fprintf('SNE:   %g\n', etime(time2, time1));
 
 
 figure
