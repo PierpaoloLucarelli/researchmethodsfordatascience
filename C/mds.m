56c183ccc5696ee5e2dd284a47199edf32b1fd5b
Pierpaolo <plucarelli.94@gmail.com>
2019-03-30 15:37:35 +0100

    mds

--- C/mds.m
+++ C/mds.m
@@ -1,13 +1,14 @@
-function data = mds(D, l, n)
+function data = mds(D, l)
+    n = size(D, 1);
     I_n = eye(n);
     e = ones(n, 1);
-    G = -0.5 * (I_n  - ((1/n)*e*e')) * D * (I_n  - ((1/n)*e*e'));
+    J = I_n  - ((1/n)*e*e');
+    G = -0.5 * J * D * J;
     [evecs,evals] = eig(G);
     [evals, idx] = sort(diag(evals), 'descend');
     % sort eigenvectors
     idx = idx(1:n);
     evecs = evecs(:,idx);
-    
     data = zeros(n, l);
     for i=1:n
         data(i,:) = sqrt(evals(i)) * evecs(i) * i;
--- C/tests.m
+++ C/tests.m
@@ -1,6 +1,8 @@
 data = readtable('IRIS.csv');
-% remove the column names and non numeric columns
+% data = ratings;
+% remove the column names and nasdon numeric columns
 data = table2array(data(:,1:size(data, 2)-1));
+data = data(1:5, :);
 [pca_data1, eigVecs] = mypca(data, 2);
 [pca_data2, eigVecs2] = gramPca(data, 2);
 [pca_data3, eigVecs3] = snapPca(data, 2, 0.9);
@@ -12,11 +14,11 @@ data = table2array(data(:,1:size(data, 2)-1));
 D = zeros(size(data, 1), size(data, 1));
 for i=1:size(data, 1)
     for j=1:size(data, 1)
-        D(i,j) = sqrt(sum((data(i,:) - data(j,:)) .^ 2));
+        D(i,j) = sum((data(i,:) - data(j,:)).^ 2);
     end
 end
 
-mds_data= mds(D, 2, size(data, 1));
+mds_data= mds(D, 2);
 scatter(mds_data(:,1), mds_data(:,2))
 % notes
 % discuss complexity 
