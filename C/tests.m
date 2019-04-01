4f8d97425401f5e192e5bda3aa8e80969fb1e2f5
NavinLR <lr.navin@yahoo.com>
2019-04-01 11:01:27 +0200

    Celebeigen faces Viz

--- C/tests.m
+++ C/tests.m
@@ -1,4 +1,6 @@
-data_set_choice = 2;
+data_set_choice = 3;
+% Choice 3 for Celeb faces --- dataset
+
 
 if data_set_choice == 0
     load fisheriris;
@@ -12,51 +14,63 @@ elseif data_set_choice == 2
     importfile('cifar-10.mat');
     data  = double(data); 
     label = double(labels);
+elseif data_set_choice == 3
+    data = read_celeb_img(100);
 else
     heart_data = readtable('heart.csv');
     data = table2array(heart_data(:,1:end-1));
     label = table2array(heart_data(:,end));
 end    
 
-%data  = data(10,:);
-
 time1   = clock;
-[pca_data1, eigVecs] = mypca(data, 2);
-time2   = clock;
-fprintf('Covar-PCA:   %g\n', etime(time2, time1));
-
-time1   = clock;
-[pca_data2, eigVecs2] = gramPca(data, 2);
+[pca_data2, eigVecs2] = gramPca(data, 100);
 time2   = clock;
 fprintf('Gram-PCA:   %g\n', etime(time2, time1));
+imageArray = viewEigenImages(data, eigVecs2(:,1));
+figure
+imshow(imageArray);
+
+time1   = clock;
+[pca_data1, eigVecs] = mypca(data, 100);
+time2   = clock;
+fprintf('Covar-PCA:   %g\n', etime(time2, time1));
+imageArray = viewEigenImages(data, eigVecs(:,1));
+figure
+imshow(imageArray);
 
 time1   = clock;
 [pca_data3, eigVecs3] = snapPca(data, 2, 1);
 time2   = clock;
 fprintf('Snap-PCA:   %g\n', etime(time2, time1));
+imageArray = viewEigenImages(data, eigVecs3(:,1));
+figure
+imshow(imageArray);
 
 time1   = clock;
 [pca_data4, eigVecs4] = nystrom(data, 2);
 time2   = clock;
 fprintf('Nyst-PCA:   %g\n', etime(time2, time1));
-
-time1   = clock;
-sne_op = tsne(data);
-time2   = clock;
-fprintf('SNE:   %g\n', etime(time2, time1));
-
-
+imageArray = viewEigenImages(data, eigVecs4(:,1));
 figure
-gscatter(pca_data1(:,1), pca_data1(:,2), label);
+imshow(imageArray);
 
-figure
-gscatter(pca_data2(:,1), pca_data2(:,2), label);
+% time1   = clock;
+% sne_op = tsne(data);
+% time2   = clock;
+% fprintf('SNE:   %g\n', etime(time2, time1));
 
-figure
-gscatter(pca_data3(:,1), pca_data3(:,2), label);
 
-figure
-gscatter(pca_data4(:,1), pca_data4(:,2), label);
-
-figure
-gscatter(sne_op(:,1), sne_op(:,2), label);
+% figure
+% gscatter(pca_data1(:,1), pca_data1(:,2), label);
+% 
+% figure
+% gscatter(pca_data2(:,1), pca_data2(:,2), label);
+% 
+% figure
+% gscatter(pca_data3(:,1), pca_data3(:,2), label);
+% 
+% figure
+% gscatter(pca_data4(:,1), pca_data4(:,2), label);
+% 
+% figure
+% gscatter(sne_op(:,1), sne_op(:,2), label);
---
+++ C/viewEigenImages.m
@@ -0,0 +1,33 @@
+function [showArray] = viewEigenImages(data, em1)
+    
+    eigVec=[];
+    for i=1:size(data,1)
+        x = data(i,:)';
+        imgData = rescale(x .* em1);
+        eigVec=[eigVec imgData];
+    end
+    
+
+    
+    imageArray=[];
+    rowcount=10;
+    colcount=10;
+    
+    for i=1:size(eigVec,2)
+        imgMat = reshape(eigVec(:,i),floor(sqrt(size(eigVec,1))),floor(sqrt(size(eigVec,1))));
+        imageArray = [imageArray imgMat];
+    end
+    showArray = zeros(rowcount*50, colcount*50);
+    
+    startCounter=1;
+    endCounter=50;
+
+    for x=1:50:rowcount*50
+        for y=1:50:colcount*50
+            showArray(x:(x-1)+50,y:(y-1)+50) = imageArray(:,startCounter:endCounter);
+            startCounter = startCounter + 50;
+            endCounter   = endCounter + 50;
+        end
+    end
+    %imageArray = reshape(imageArray, 10 * 50,  10 * 50);
+end
